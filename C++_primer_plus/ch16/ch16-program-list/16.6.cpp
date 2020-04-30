dd24ff427cb62229444ef21b1b268e9c8893f4e7
712 <2294580367@qq.com>
2020-04-30 19:55:34 +0800

    712

---
+++ C++_primer_plus/ch16/ch16-program-list/16.6.cpp
@@ -0,0 +1,26 @@
+// fowl.cpp  -- shared_ptr a poor choice
+#include <iostream>
+#include <string>
+#include <memory>
+
+int main()
+{
+    using namespace std;
+    shared_ptr<string> films[5] =
+    {
+        shared_ptr<string> (new string("Fowl Balls")),
+        shared_ptr<string> (new string("Duck Walks")),
+        shared_ptr<string> (new string("Chicken Runs")),
+        shared_ptr<string> (new string("Turkey Errors")),
+        shared_ptr<string> (new string("Goose Eggs"))
+    };
+    shared_ptr<string> pwin;
+    pwin = films[2];   // films[2] loses ownership
+
+    cout << "The nominees for best avian baseball film are\n";
+    for (int i = 0; i < 5; i++)
+        cout << *films[i] << endl;
+    cout << "The winner is " << *pwin << "!\n";
+    // cin.get();
+    return 0;
+}
